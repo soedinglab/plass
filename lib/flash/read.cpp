fb3f2951ad3062709789bb4f23a2c1e6820539bb
Milot Mirdita <milot@mirdita.de>
2021-03-23 13:53:16 +0100

    Refactor flash's reverse_with_mapping function to allow better inlining

--- lib/flash/read.cpp
+++ lib/flash/read.cpp
@@ -1,41 +1,40 @@
 #include "read.h"
 
-
-
 //note: N->N, S->S, W->W, U->A, T->A
 static const char* complement_tab =
         "................................................................"
-                ".TVGH..CD..M.KN...YSAABW.R.......tvgh..cd..m.kn...ysaabw.r......"
-                "................................................................"
-                "................................................................";
+        ".TVGH..CD..M.KN...YSAABW.R.......tvgh..cd..m.kn...ysaabw.r......"
+        "................................................................"
+        "................................................................";
 
 /* Complements a canonical ASCII base (A, C, G, T, N). */
-static inline char
-complement(char c)
-{
+struct complement {
+    char operator()(char c) {
+        return complement_tab[(unsigned char)c];
+    }
+};
 
-    return complement_tab[(unsigned char)c];
-}
 
-
-static inline char
-identity_mapping(char c)
-{
-    return c;
-}
+struct identity_mapping {
+    char operator()(char c) {
+        return c;
+    }
+};
 
 /* Reverse a sequence of @len chars, applying the mapping function @map_char to
  * each, including the middle char if @len is odd.  */
+template<typename T>
 static inline void
-reverse_with_mapping(char *p, size_t len, char (*map_char)(char))
+reverse_with_mapping(char *p, size_t len)
 {
+    T t;
     char tmp;
     char *pp = p + len;
     while (pp > p) {
         --pp;
         tmp = *p;
-        *p = (*map_char)(*pp);
-        *pp = (*map_char)(tmp);
+        *p = t(*pp);
+        *pp = t(tmp);
         ++p;
     }
 }
@@ -44,6 +43,6 @@ reverse_with_mapping(char *p, size_t len, char (*map_char)(char))
 extern void
 reverse_complement(struct read *r)
 {
-    reverse_with_mapping(r->seq, r->seq_len, complement);
-    reverse_with_mapping(r->qual, r->seq_len, identity_mapping);
+    reverse_with_mapping<complement>(r->seq, r->seq_len);
+    reverse_with_mapping<identity_mapping>(r->qual, r->seq_len);
 }
